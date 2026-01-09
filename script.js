0b6f2b8d9ac00df8d9c40a8f8ef7fe19126a3c53
Malik-Idi <malikidielhadjmoumouni@gmail.com>
2026-01-09 17:15:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const generateBtn = document.getElementById("generatePlan");
     const editor = document.getElementById("editor");
+    const preview = document.getElementById("preview");
+
+    function updatePreview() {
+        preview.innerHTML = "";
+
+        const lines = editor.value.split("\n");
+
+        lines.forEach(line => {
+            if (line.trim() !== "") {
+                const p = document.createElement("p");
+                p.textContent = line;
+                preview.appendChild(p);
+            }
+        });
+    }
 
     generateBtn.addEventListener("click", function () {
         editor.value =
@@ -18,7 +33,10 @@ B. Deuxième conséquence
 IV. Solutions
 
 V. Conclusion`;
+
+        updatePreview();
     });
 
-});
+    editor.addEventListener("input", updatePreview);
 
+});
