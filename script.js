ee370767be0a93020e52d8ca2d4262e94d5df818
Malik-Idi <malikidielhadjmoumouni@gmail.com>
2026-01-12 19:26:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,43 @@ V. Conclusion`;
     editor.addEventListener("input", updatePreview);
 
 });
+
+/* PAGE WORD A4 */
+.preview-sheet {
+    position: relative;
+    width: 210mm;
+    min-height: 297mm;
+    margin: auto;
+    padding: 25mm 20mm;
+    background: white;
+    font-family: "Times New Roman", serif;
+}
+
+/* EN-TÊTE */
+.page-header {
+    border: 2px solid #0aa64b;
+    padding: 12px;
+    text-align: center;
+    margin-bottom: 25px;
+    background: #f3fff7;
+}
+
+#doc-title {
+    font-size: 20px;
+    font-weight: bold;
+    text-transform: uppercase;
+}
+
+/* CONTENU */
+#preview-content p {
+    margin: 6px 0;
+}
+
+/* PIED DE PAGE */
+.page-footer {
+    position: absolute;
+    bottom: 15mm;
+    right: 20mm;
+    font-size: 12px;
+    color: #555;
+}
