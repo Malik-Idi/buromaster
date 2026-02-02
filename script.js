f08929d7ac0261a1827270b8858cd8383036fa7c
Malik-Idi <malikidielhadjmoumouni@gmail.com>
2026-02-02 13:04:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -400,7 +400,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 1. N'utilise aucune formule orale (pas de "Mesdames et Messieurs").
                 2. Adopte un ton neutre, informatif et scolaire.
                 3. Le texte doit être compréhensible par un élève.
-                4. L'introduction doit capter l'attention avec une accroche, définir le sujet via une problématique claire et annoncer les étapes du plan.
+                4. L'introduction doit capter l'attention avec une accroche, définir le sujet de manière claire (sans formuler explicitement une question) et annoncer les grandes étapes du plan.
                 5. Utilise des phrases claires et structurées, sans style littéraire excessif.
                 6. Organise le texte en plusieurs paragraphes logiques.
 
